33b9ce8d75cb2f44a55df95fa38ee44447183481
Riccardo Chimisso <riccardo.chimisso@gmail.com>
2022-04-19 16:17:20 +0200

    Fixed vertical slabs not being rendered until chunk was reloaded.

--- src/main/java/crystalspider/justverticalslabs/VerticalSlabBlockEntity.java
+++ src/main/java/crystalspider/justverticalslabs/VerticalSlabBlockEntity.java
@@ -3,9 +3,11 @@ package crystalspider.justverticalslabs;
 import net.minecraft.core.BlockPos;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.nbt.NbtUtils;
+import net.minecraft.network.Connection;
 import net.minecraft.network.protocol.Packet;
 import net.minecraft.network.protocol.game.ClientGamePacketListener;
 import net.minecraft.network.protocol.game.ClientboundBlockEntityDataPacket;
+import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraftforge.client.model.data.IModelData;
@@ -48,6 +50,15 @@ public class VerticalSlabBlockEntity extends BlockEntity {
   public Packet<ClientGamePacketListener> getUpdatePacket() {
     return ClientboundBlockEntityDataPacket.create(this);
   } 
+
+  @Override
+  public void onDataPacket(Connection net, ClientboundBlockEntityDataPacket pkt) {
+    super.onDataPacket(net, pkt);
+    requestModelDataUpdate();
+    if (level != null && level.isClientSide) {
+      level.sendBlockUpdated(getBlockPos(), getBlockState(), getBlockState(), Block.UPDATE_CLIENTS);
+    }
+  }
 
   @Override
   public IModelData getModelData() {
