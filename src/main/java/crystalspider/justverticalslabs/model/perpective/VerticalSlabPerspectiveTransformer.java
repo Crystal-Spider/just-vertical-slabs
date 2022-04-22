567c4dedf73dd344f22170dbb2d1d19004811d0a
Riccardo Chimisso <riccardo.chimisso@gmail.com>
2022-04-22 18:05:47 +0200

    Added Javadoc.

--- src/main/java/crystalspider/justverticalslabs/model/perpective/VerticalSlabPerspectiveTransformer.java
+++ src/main/java/crystalspider/justverticalslabs/model/perpective/VerticalSlabPerspectiveTransformer.java
@@ -9,7 +9,13 @@ import java.util.Map;
 import net.minecraft.client.renderer.block.model.ItemTransforms.TransformType;
 import net.minecraftforge.common.model.TransformationHelper;
 
+/**
+ * Holder of {@link Transformation perspective transformations} for Vertical Slab Items.
+ */
 public class VerticalSlabPerspectiveTransformer {
+  /**
+   * Map of all the Vertical Slab Item {@link Transformation transformations} associated with their respective {@link TransformType}.
+   */
   private static final Map<TransformType, Transformation> TRANSFORMATIONS = Map.ofEntries(
     entry(TransformType.GUI, getTransformation(new Vector3f(0.1F, -0.05F, 0), new Vector3f(30, 45, 0), new Vector3f(0.625F, 0.625F, 0.625F))),
     entry(TransformType.GROUND, getTransformation(new Vector3f(0, 0.015F, 0.075F), Vector3f.ZERO, new Vector3f(0.25F, 0.25F, 0.25F))),
@@ -17,14 +23,27 @@ public class VerticalSlabPerspectiveTransformer {
     entry(TransformType.FIRST_PERSON_LEFT_HAND, getTransformation(Vector3f.ZERO, new Vector3f(0, 315, 0), new Vector3f(0.4F, 0.4F, 0.4F))),
     entry(TransformType.FIRST_PERSON_RIGHT_HAND, getTransformation(Vector3f.ZERO, new Vector3f(0, 135, 0), new Vector3f(0.4F, 0.4F, 0.4F))),
     entry(TransformType.THIRD_PERSON_LEFT_HAND, getTransformation(new Vector3f(0F, 0.175F, 0), new Vector3f(75, 315, 0), new Vector3f(0.375F, 0.375F, 0.375F))),
-    entry(TransformType.THIRD_PERSON_RIGHT_HAND, getTransformation(new Vector3f(0F, 0.175F, 0), new Vector3f(75, 135, 0), new Vector3f(0.375F, 0.375F, 0.375F))),
-    entry(TransformType.NONE, getTransformation(Vector3f.ZERO, Vector3f.ZERO, new Vector3f(1, 1, 1)))
+    entry(TransformType.THIRD_PERSON_RIGHT_HAND, getTransformation(new Vector3f(0F, 0.175F, 0), new Vector3f(75, 135, 0), new Vector3f(0.375F, 0.375F, 0.375F)))
   );
 
+  /**
+   * Returns a new {@link Transformation} given the translation, rotation and scale {@link Vector3f vectors}.
+   * 
+   * @param translation - translation {@link Vector3f}.
+   * @param rotation - rotation {@link Vector3f}, in degrees.
+   * @param scale - scale {@link Vector3f}.
+   * @return a new {@link Transformation}.
+   */
   private static Transformation getTransformation(Vector3f translation, Vector3f rotation, Vector3f scale) {
     return new Transformation(translation, TransformationHelper.quatFromXYZ(rotation, true), scale, null);
   }
 
+  /**
+   * Returns the correct Vertical Slab Item {@link Transformation} given the {@link TransformType}.
+   * 
+   * @param transformType - {@link TransformType}.
+   * @return one of {@link #TRANSFORMATIONS Vertical Slab Item Transformations}.
+   */
   public static Transformation getTransformation(TransformType transformType) {
     return TRANSFORMATIONS.getOrDefault(transformType, Transformation.identity());
   }
