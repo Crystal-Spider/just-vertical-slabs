ab9adf101efb9c9d793902c94f9832b67f909de5
Riccardo Chimisso <riccardo.chimisso@gmail.com>
2022-04-23 22:32:32 +0200

    Added new item to todo list.

--- src/main/java/crystalspider/justverticalslabs/model/vertex/VerticalSlabVertexTransformer.java
+++ src/main/java/crystalspider/justverticalslabs/model/vertex/VerticalSlabVertexTransformer.java
@@ -12,6 +12,7 @@ import net.minecraftforge.client.model.pipeline.VertexTransformer;
  * TODO:
  * Crop textures rather than stretching them.
  * Emit light if referred block emits light.
+ * Fix breaking progress not being shown.
  */
 public class VerticalSlabVertexTransformer extends VertexTransformer {
   /**
