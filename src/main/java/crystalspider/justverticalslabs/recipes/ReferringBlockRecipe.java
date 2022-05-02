5111efa7bbca7dd2c58c69abafbc3fc0a3552fce
Riccardo Chimisso <riccardo.chimisso@gmail.com>
2022-05-02 10:47:15 +0200

    Fixed ReferringBlockRecipe logic.

--- src/main/java/crystalspider/justverticalslabs/recipes/ReferringBlockRecipe.java
+++ src/main/java/crystalspider/justverticalslabs/recipes/ReferringBlockRecipe.java
@@ -13,35 +13,18 @@ public class ReferringBlockRecipe extends VerticalSlabRecipe {
   public static final String ID = "referring_block_recipe";
   private static final ResourceLocation RESOURCE_LOCATION = VerticalSlabUtils.getResourceLocation(ID);
 
-  private int firstIndex = 0;
-
   public ReferringBlockRecipe() {
     super(2, 1);
   }
 
   @Override
   public boolean matches(CraftingContainer craftingContainer, Level level) {
-    boolean matchFound = false;
-    int containerWidth = craftingContainer.getWidth();
-    for (int w = 0; w < containerWidth - 1; w++) {
-      for (int h = 0; h < craftingContainer.getHeight(); h++) {
-        int index = w + h * containerWidth;
-        if (isVerticalSlab(craftingContainer.getItem(index)) && isVerticalSlab(craftingContainer.getItem(index + 1))) {
-          if (matchFound) {
-            matchFound = false;
-            break;
-          }
-          matchFound = true;
-          firstIndex = index;
-        }
-      }
-    }
-    return matchFound;
+    return getMatchIndex(craftingContainer) != -1;
   }
 
   @Override
   public ItemStack assemble(CraftingContainer craftingContainer) {
-    return VerticalSlabUtils.getReferringBlockState(craftingContainer.getItem(firstIndex)).getBlock().asItem().getDefaultInstance();
+    return VerticalSlabUtils.getReferringBlockState(craftingContainer.getItem(getMatchIndex(craftingContainer))).getBlock().asItem().getDefaultInstance();
   }
 
   @Override
@@ -57,6 +40,30 @@ public class ReferringBlockRecipe extends VerticalSlabRecipe {
   @Override
   public RecipeSerializer<ReferringBlockRecipe> getSerializer() {
     return JustVerticalSlabsLoader.REFERRING_BLOCK_RECIPE_SERIALIZER.get();
+  }
+
+  private int getMatchIndex(CraftingContainer craftingContainer) {
+    boolean correctPattern = true;
+    int matchIndex = -1, containerWidth = craftingContainer.getWidth();
+    for (int h = 0; h < containerWidth && correctPattern; h++) {
+      for (int w = 0; w < craftingContainer.getHeight() && correctPattern; w++) {
+        int index = w + h * containerWidth;
+        if (isVerticalSlab(craftingContainer.getItem(index))) {
+          if (isVerticalSlab(craftingContainer.getItem(index + 1))) {
+            if (matchIndex == -1) {
+              matchIndex = index;
+            } else {
+              matchIndex = -1;
+              correctPattern = false;
+            }
+          } else if (matchIndex != index - 1) {
+            matchIndex = -1;
+            correctPattern = false;
+          }
+        }
+      }
+    }
+    return matchIndex;
   }
 
   public static class Serializer extends VerticalSlabRecipe.Serializer<ReferringBlockRecipe> {
