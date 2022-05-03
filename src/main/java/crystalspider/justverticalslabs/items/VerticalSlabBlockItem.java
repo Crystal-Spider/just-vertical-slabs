433395f399c13ea4b3a2c81ba59123dd584d3d66
Riccardo Chimisso <riccardo.chimisso@gmail.com>
2022-05-03 13:59:58 +0200

    Added burn time.

--- src/main/java/crystalspider/justverticalslabs/items/VerticalSlabBlockItem.java
+++ src/main/java/crystalspider/justverticalslabs/items/VerticalSlabBlockItem.java
@@ -7,21 +7,34 @@ import crystalspider.justverticalslabs.utils.VerticalSlabUtils;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.NonNullList;
 import net.minecraft.nbt.CompoundTag;
+import net.minecraft.network.chat.Component;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.crafting.RecipeType;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraftforge.common.ForgeHooks;
 
+/**
+ * Vertical Slab {@link BlockItem}.
+ */
 public class VerticalSlabBlockItem extends BlockItem {
 
   public VerticalSlabBlockItem(Block block, Properties properties) {
     super(block, properties);
   }
 
+  /**
+   * In the given {@link CreativeModeTab} adds as many Vertical Slab {@link ItemStack ItemStacks} as there are for Slabs.
+   * 
+   * @param creativeModeTab - {@link CreativeModeTab} in the creative inventory.
+   * @param itemStacks - {@link NonNullList list} of {@link ItemStack ItemStacks} in the given {@link CreativeModeTab}.
+   */
   @Override
   public void fillItemCategory(CreativeModeTab creativeModeTab, NonNullList<ItemStack> itemStacks) {
     if (this.allowdedIn(creativeModeTab) && JustVerticalSlabsLoader.slabMap != null) {
@@ -31,9 +44,30 @@ public class VerticalSlabBlockItem extends BlockItem {
     }
   }
 
+  @Override
+  public ItemStack getDefaultInstance() {
+    return VerticalSlabUtils.getDefaultInstance();
+  }
+
+  @Override
+  public int getBurnTime(ItemStack itemStack, @Nullable RecipeType<?> recipeType) {
+    BlockState referringBlockState = VerticalSlabUtils.getReferringBlockState(itemStack);
+    if (referringBlockState != null) {
+      return ForgeHooks.getBurnTime(JustVerticalSlabsLoader.blockMap.get(referringBlockState.getBlock().asItem()).getDefaultInstance(), recipeType);
+    }
+    return super.getBurnTime(itemStack, recipeType);
+  }
+
   /**
    * Forces BlockEntity update on client.
    * Refer to {@link BlockItem#updateCustomBlockEntityTag(Level, Player, BlockPos, ItemStack)} for implementation.
+   * 
+   * @param pos
+   * @param level
+   * @param player
+   * @param itemStack
+   * @param state
+   * @return whether the {@link BlockEntity} NBTs were updated.
    */
   @Override
   protected boolean updateCustomBlockEntityTag(BlockPos pos, Level level, @Nullable Player player, ItemStack itemStack, BlockState state) {
